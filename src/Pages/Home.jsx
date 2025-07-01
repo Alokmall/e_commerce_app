efc4106ecc5cce8d9566e754f79884f92380fbaa
Alok <alokmall573gkp@gmail.com>
2025-07-01 22:14:50 +0530

    Fix: Correct path to Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { ProductContext } from '../context/ProductContext';
+import { ProductContext } from '../context/Productcontext';
 import ProductList from '../components/ProductList';
 
 export default function Home() {
